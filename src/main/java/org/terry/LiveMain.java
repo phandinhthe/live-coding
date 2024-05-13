9f23d7e5b6b80f85be912523931efb63dd949912
phandinhthe <phandinhthe1991@gmail.com>
2024-05-13 14:47:48 +0700

    Reset live-main

--- src/main/java/org/terry/LiveMain.java
+++ src/main/java/org/terry/LiveMain.java
@@ -8,72 +8,7 @@ public class LiveMain {
 	}
 
 	public void test() {
-		int[][] grid;
-		int outptut;
-		int actual;
 
-		grid = new int[][]{{0, 0, 1, 1}, {1, 0, 1, 0}, {1, 1, 0, 0}};
-		outptut = 39;
-		actual = matrixScore(grid);
-		assertEquals(outptut, actual);
 
-		grid = new int[][]{{1, 1, 1}, {1, 1, 0}, {1, 1, 0}};
-		outptut = 20;
-		actual = matrixScore(grid);
-		assertEquals(outptut, actual);
-
-		grid = new int[][]{{0}};
-		outptut = 1;
-		actual = matrixScore(grid);
-		assertEquals(outptut, actual);
-
-	}
-
-	public int matrixScore(int[][] grid) {
-		int rowSize = grid.length;
-		int colSize = grid[0].length;
-
-		for (int i = 0; i < rowSize; i++) {
-			if (grid[i][0] == 0) {
-				// flip cols
-				for (int j = 0; j < colSize; j++) {
-					grid[i][j] = Math.abs(grid[i][j] - 1);
-				}
-			}
-		}
-
-		for (int i = 0; i < colSize; i++) {
-			int totalOneBit = 0;
-			for (int j = 0; j < rowSize; j++) {
-				totalOneBit += grid[j][i];
-			}
-			int totalZeroBit = rowSize - totalOneBit;
-			if (totalOneBit > totalZeroBit) continue;
-
-			for (int j = 0; j < rowSize; j++) {
-				grid[j][i] = Math.abs(grid[j][i] - 1);
-			}
-		}
-
-		int res = 0;
-		for (int[] rows : grid) {
-			int pow = colSize-1;
-			for (int col : rows) {
-				if (col == 1) res += Math.pow(2, pow);
-				pow --;
-			}
-		}
-		return res;
-	}
-
-	private void print(int[][] grid) {
-		for (int i = 0; i < grid.length; i++) {
-			for (int j = 0; j < grid[0].length; j++) {
-				System.out.printf("%3d", grid[i][j]);
-			}
-			System.out.println();
-		}
-
-		System.out.println();
 	}
 }
