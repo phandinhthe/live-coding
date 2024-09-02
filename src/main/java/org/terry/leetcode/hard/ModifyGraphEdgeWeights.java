b25ffc94be61998c736630383a932f640c71d6cb
phandinhthe <phandinhthe1991@gmail.com>
2024-09-02 10:26:42 +0700

    Leetcode 2699: update tuition

--- src/main/java/org/terry/leetcode/hard/ModifyGraphEdgeWeights.java
+++ src/main/java/org/terry/leetcode/hard/ModifyGraphEdgeWeights.java
@@ -4,6 +4,21 @@ import org.junit.jupiter.api.Assertions;
 
 import java.util.*;
 
+/**
+ * Leetcode 2699. Modify Graph Edge Weights.
+ * Tuition:
+ * - Initialize graph, edge weight == -1 will be ignored.
+ * - Find the shortest path, using Djikstra, if:
+ * - - shortest path == target => set all edge weight -1 = MAX, return edges.
+ * - - shortest path != target => iterate over all edges having weight -1, set edge = 1, find the shortest path
+ * - - - if result == target, set all edge weight -1 to MAX and return edges.
+ * - - - else the shortest path != target, update edge += (target - shortest path), return edges.
+ * <p>
+ * n = edges.length
+ * COmplexity: O(nlogn)
+ * Space: O(n*n)
+ */
+
 public class ModifyGraphEdgeWeights {
     public static void main(String[] args) {
         new ModifyGraphEdgeWeights().run();
